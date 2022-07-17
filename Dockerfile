098b417adffb02a1200d9813db931637abebe910
CamilaMusina <56271888+CamilaMusina@users.noreply.github.com>
2022-07-17 17:29:42 +0300

    Initial Commit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM se2beta/vaadin-builder:ubi
+FROM openjdk:17
 ARG JAR_FILE=target/*.jar
 ENV BOT_NAME=teko_practice_tgbot
 ENV BOT_TOKEN=5555254173:AAGE6liapeVMENnvRTl_4gPmlGrxWdzpzPI
+ENV BOT_DB_USERNAME=tgbot_db_user
+ENV BOT_DB_PASSWORD=tgbot_db_password
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java", "-Dbot.username=${BOT_NAME}", "-Dbot.token=${BOT_TOKEN}", "-jar", "/app.jar"]
+ENTRYPOINT ["java", "-Dspring.datasource.password=${BOT_DB_PASSWORD}", "-Dbot.username=${BOT_NAME}", "-Dbot.username=${BOT_NAME}", "-Dbot.token=${BOT_TOKEN}", "-jar", "/app.jar"]
--- src/main/java/com/example/telegrambot/command/StartCommand.java
+++ src/main/java/com/example/telegrambot/command/StartCommand.java
@@ -1,20 +1,39 @@
 package com.example.telegrambot.command;
 
+import com.example.telegrambot.repository.entity.TelegramUser;
+import com.example.telegrambot.service.TelegramUserService;
 import com.example.telegrambot.command.Command;
 import com.example.telegrambot.service.SendBotMessageService;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class StartCommand implements Command {
     private final SendBotMessageService sendBotMessageService;
+    private final TelegramUserService telegramUserService;
 
-    public final static String START_MESSAGE = "Добавить список команд бота и приветствие";
+    public final static String START_MESSAGE = "Привет, я teko_practice_bot/n Введите ID компании";
 
-    public StartCommand(SendBotMessageService sendBotMessageService) {
+    public StartCommand(SendBotMessageService sendBotMessageService, TelegramUserService telegramUserService) {
         this.sendBotMessageService = sendBotMessageService;
+        this.telegramUserService = telegramUserService;
     }
 
     @Override
     public void execute(Update update) {
-        sendBotMessageService.sendMessage(update.getMessage().getChatId().toString(), START_MESSAGE);
+        String chatId = update.getMessage().getChatId().toString();
+
+        telegramUserService.findByChatId(chatId).ifPresentOrElse(
+                user -> {
+                    user.setActive(true);
+                    telegramUserService.save(user);
+                },
+                () -> {
+                    TelegramUser telegramUser = new TelegramUser();
+                    telegramUser.setActive(true);
+                    telegramUser.setChat_id(chatId);
+                    telegramUserService.save(telegramUser);
+                }
+        );
+
+        sendBotMessageService.sendMessage(chatId, START_MESSAGE);
     }
 }
